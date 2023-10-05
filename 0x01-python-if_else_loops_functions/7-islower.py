3175d280884852587e9934f0dff9299037d6e8a6
Clinton Ogolla <clintonmcogolla@gmail.com>
2023-10-05 01:43:57 +0000

    if else

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(d):
-    #checks if a  character is lowercase
+    '''checks if a  character is lowercase'''
     if ord(d) >= 97 and ord(d) <= 122:
         return True
     else:
