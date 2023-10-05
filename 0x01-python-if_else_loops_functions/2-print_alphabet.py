654387506a92e53ec1ee6ceef362ade034cdf8b5
Clinton Ogolla <clintonmcogolla@gmail.com>
2023-10-05 02:53:07 +0000

    if else

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """Prints the alphabet in lowercase"""
 
-for l in range(97, 123):
-    print("{}".format(chr(l)), end="")
+for ler in range(97, 123):
+    print("{}".format(chr(ler)), end="")
