984fc0062225c6a53d8b111ef88f33cb2f8c85e7
Clinton Ogolla <clintonmcogolla@gmail.com>
2023-10-05 01:27:29 +0000

    if else

---
+++ 0x01-python-if_else_loops_functions/100-print_tebalpha.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+d = 0
+for i in range(ord('z'), ord('a') - 1, -1):
+        print(f'{chr(i - d))', end = '')
+            d = 32 if d == 0 else 0
---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for me1 in range(10):
+    for me2 in range(me1 + 1, 10):
+        if me1 == 8 and me2 == 9:
+            print(f'{me1}{me2}')
+        else:
+            print(f'{me1}{me2}, ', end = '')
