b3da9713a40a9ca7636ca46388d4c97acfcad2df
Clinton Ogolla <clintonmcogolla@gmail.com>
2023-10-13 10:01:57 +0000

    two digit combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
-for me1 in range(10):
-    for me2 in range(me1 + 1, 10):
-        if me1 == 8 and me2 == 9:
-            print(f'{me1}{me2}')
-        else:
-            print("{:me1}{:me2}".format(me1. me2))
+for x in range(10):
+    for y in range(x + 1, 10):
+        if x != y:
+            if x < 8:
+                print("{:d}{:d}, ".format(x, y), end='')
+            else:
+                print("{:d}{:d}".format(x, y))
