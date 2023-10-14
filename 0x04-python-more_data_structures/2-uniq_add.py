0cad93bfce0e5dd0e6e602435c3f9da994595061
Clinton Ogolla <clintonmcogolla@gmail.com>
2023-10-14 01:21:47 +0000

    more data structures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    my_list.sort()
-    sum = 0
-    for i in range(len(my_list)):
-        if i == 0 or my_list[i] != my_list[i - 1]:
-            sum += my_list[i]
-            return sum
+    return sum({t for t in my_list if isinstance(t, int)})
--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common = set()
-    for i in set_1:
-        if i in set_2:
-            common.add(i)
-            return common
+    return {item for item in set_1 if item in set_2}
