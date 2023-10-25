17c72736a57dd3d243dfa879aef4d3f35cc8e75e
Clinton Ogolla <clintonmcogolla@gmail.com>
2023-10-25 13:22:51 +0000

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Creates a square"""
+    def __init__(self, size):
+        """Initialize size"""
+        self.__size = size
